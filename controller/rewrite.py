b7817291bef39a248cac9e0cf9717ab0b252364c
osboxes.org <brayils.andre@gmail.com>
2018-04-24 01:50:31 -0300

    Tables done

--- controller/rewrite.py
+++ controller/rewrite.py
@@ -7,20 +7,19 @@ from core.packets import *
 class SimpleSwitchApplication(eBPFCoreApplication):
     @set_event_handler(Header.HELLO)
     def hello(self, connection, pkt):
-	server_list = ["0a02000a", "0a03000a"]
+        server_list = ["0a00020a", "0a00030a"]
         switches = [1]
-        #Rewrite Switches
-	if (pkt.dpid in switches):
-		with open('../examples/rewrite.o', 'rb') as f:
-		    print("Installing the eBPF ELF")
-		    connection.send(InstallRequest(elf=f.read()))
-		for i in range(len(server_list)):
-		    connection.send(TableEntryInsertRequest(table_name="servers", key=str(i+1).zfill(8).decode('hex'), value=server_list[i].decode('hex')))
-        #Learning Switches
+        if (pkt.dpid in switches):
+            with open('../examples/rewrite.o', 'rb') as f:
+                print("Installing the eBPF ELF")
+                connection.send(InstallRequest(elf=f.read()))
+            for i in range(len(server_list)):
+                connection.send(TableEntryInsertRequest(table_name="servers", key=struct.pack('<I', i+1), value=server_list[i].decode('hex')))
+
         else:
-		with open('../examples/learningswitch.o', 'rb') as f:
-		    print("Installing the eBPF ELF")
-		    connection.send(InstallRequest(elf=f.read()))
+            with open('../examples/learningswitch.o', 'rb') as f:
+                print("Installing the eBPF ELF")
+                connection.send(InstallRequest(elf=f.read()))
 
     @set_event_handler(Header.PACKET_IN)
     def packet_in(self, connection, pkt):
--- examples/rewrite.c
+++ examples/rewrite.c
@@ -21,33 +21,38 @@ struct bpf_map_def SEC("maps") inports = {
     .max_entries = 256,
 };
 
+const int serversNumber = 2;
 struct bpf_map_def SEC("maps") servers = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = 4,
     .value_size = sizeof(uint32_t),
-    .max_entries = 3,
+    .max_entries = serversNumber,
 };
+
+struct bpf_map_def SEC("maps") conns = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = 4,
+    .value_size = sizeof(uint64_t),
+    .max_entries = 512,
+};
+
+int isServer(uint32_t* ip);
+int hashIP(uint32_t* ip);
+int makeConnKey(uint32_t* src, uint32_t* dst, uint32_t* srv);
+uint64_t packIpAddr(uint32_t* src, uint32_t* dst);
 
 uint64_t prog(struct packet *pkt)
 {	
 
-    #if BYTE_ORDER == LITTLE_ENDIAN
-        uint32_t server = 0x0a03000a;
-        uint32_t host = 0x0a02000a;
-    #endif
-    #if BYTE_ORDER == BIG_ENDIAN
-        uint32_t server = 0x0a00030a;
-        uint32_t host = 0x0a00020a;
-    #endif
-
     //ARP PACK
     if (pkt->eth.h_proto == ARP_PROTOCOL) {
+
 	struct arphdr *arp = (struct arphdr *)(((uint8_t *)&pkt->eth) + ETH_HLEN);
-	bpf_notify(1, &arp->ar_hrd, sizeof(uint16_t));
+	/*bpf_notify(1, &arp->ar_hrd, sizeof(uint16_t));
         bpf_notify(1, &arp->ar_pro, sizeof(uint16_t));
         bpf_notify(1, &arp->ar_hrdlen, sizeof(uint8_t));
         bpf_notify(1, &arp->ar_prolen, sizeof(uint8_t));
-        bpf_notify(1, &arp->ar_op, sizeof(uint16_t));
+        bpf_notify(1, &arp->ar_op, sizeof(uint16_t));*/
 
 	uint8_t *src_eth, *src_ip, *dst_eth, *dst_ip;
         uint16_t len = pkt->metadata.length;
@@ -55,39 +60,58 @@ uint64_t prog(struct packet *pkt)
         src_ip = src_eth + arp->ar_hrdlen;
         dst_eth = src_ip + arp->ar_prolen;
         dst_ip = dst_eth + arp->ar_hrdlen;
-	bpf_notify(1, src_eth, arp->ar_hrdlen);
+	/*bpf_notify(1, src_eth, arp->ar_hrdlen);
         bpf_notify(1, src_ip, arp->ar_prolen);
         bpf_notify(1, dst_eth, arp->ar_hrdlen);
         bpf_notify(1, dst_ip, arp->ar_prolen);
-        bpf_notify(1, &pkt->metadata.length, sizeof(uint16_t));
+        bpf_notify(1, &pkt->metadata.length, sizeof(uint16_t));*/
 
-	if ((*(uint32_t *)dst_ip) == server){
+	if (isServer(dst_ip)){//And src not a server
+		//Hash Ip src and get corresponding server
+		uint32_t i = hashIP(src_ip);
+		uint32_t* p = &i;
+		uint32_t* g;
+		bpf_map_lookup_elem(&servers, p, &g);
+		bpf_notify(1, g, sizeof(uint32_t));
+		//make a conection key to store information
+		uint32_t key = makeConnKey(src_ip,dst_ip,g);
+		uint64_t conval = packIpAddr (src_ip,dst_ip);
+		uint64_t* conp;
+		//store ip header by key
+		bpf_map_update_elem(&conns, &key, &conval, 0);
+		bpf_map_lookup_elem(&conns, &key, &conp);
+		bpf_notify(1, (uint32_t*)conp, sizeof(uint32_t));//dst
+		bpf_notify(1, (uint32_t*)conp + 1, sizeof(uint32_t));//src
+	}
+
+
+	//if ((*(uint32_t *)dst_ip) == server){
             //bpf_notify(1, dst_ip, arp->ar_prolen);
-	    (*(uint32_t *)dst_ip) = host;
-            bpf_notify(1, dst_ip, arp->ar_prolen);
-        }
-        if ((*(uint32_t *)src_ip) == host){
+	    //(*(uint32_t *)dst_ip) = host;
+            //bpf_notify(1, dst_ip, arp->ar_prolen);
+       // }
+        //if ((*(uint32_t *)src_ip) == host){
             //bpf_notify(1, src_ip, arp->ar_prolen);
-	    (*(uint32_t *)src_ip) = server;
-            bpf_notify(1, src_ip, arp->ar_prolen);
-        }
+	    //(*(uint32_t *)src_ip) = server;
+            //bpf_notify(1, src_ip, arp->ar_prolen);
+        //}
         //bpf_notify(1, &arppkg->ar_op + sizeof(uint16_t) + arppkg->ar_hrdlen - 4, arppkg->ar_prolen);
     }
     //IP PACK
     else if (pkt->eth.h_proto == IP_PROTOCOL) {
         struct ip *ipv4 = (struct ip *)(((uint8_t *)&pkt->eth) + ETH_HLEN);
-	if ((*(uint32_t *)&ipv4->ip_dst.s_addr) == server){
-            bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
-	    (*(uint32_t *)&ipv4->ip_dst.s_addr) = host;
-            (*(uint16_t *)&ipv4->ip_sum) += ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
-            bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
-        }
-        if ((*(uint32_t *)&ipv4->ip_src.s_addr) == host){
-            bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
-	    (*(uint32_t *)&ipv4->ip_src.s_addr) = server;
-            (*(uint16_t *)&ipv4->ip_sum) -= ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
-            bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
-        }
+	//if ((*(uint32_t *)&ipv4->ip_dst.s_addr) == server){
+            //bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
+	    //(*(uint32_t *)&ipv4->ip_dst.s_addr) = host;
+            //(*(uint16_t *)&ipv4->ip_sum) += ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
+            //bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
+        //}
+        //if ((*(uint32_t *)&ipv4->ip_src.s_addr) == host){
+            //bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
+	    //(*(uint32_t *)&ipv4->ip_src.s_addr) = server;
+            //(*(uint16_t *)&ipv4->ip_sum) -= ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
+            //bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
+        //}
     }
 
     
@@ -112,4 +136,43 @@ uint64_t prog(struct packet *pkt)
 
     return *out_port;
 }
+
+int isServer(uint32_t* ip){
+
+	uint32_t i = 1;
+	uint32_t* p = &i;
+	uint32_t* g;
+	while (bpf_map_lookup_elem(&servers, p, &g) != -1){
+		i++;
+		//bpf_notify(1, dst_ip, sizeof(uint32_t));
+		if ((*g) == (*ip))
+			return 1;
+	}
+	return 0;
+
+}
+
+int hashIP(uint32_t* ip){
+
+	int val = (*ip);
+	return (val>>16)%serversNumber +1;
+
+}
+
+int makeConnKey(uint32_t* src, uint32_t* dst, uint32_t* srv){
+	int val = (*src);
+	int res = (val>>16)*3;
+	/*val = (*dst);
+	res += (val>>16)*7;*/
+	val = (*srv);
+	res += (val>>16)*13;
+	return res*7;
+}
+
+uint64_t packIpAddr(uint32_t* src, uint32_t* dst){
+	uint64_t res = (*src) & 0x00000000FFFFFFFF;
+	res = (res<<32) + ((*dst) & 0x00000000FFFFFFFF);
+	return res;
+}
+
 char _license[] SEC("license") = "GPL";
