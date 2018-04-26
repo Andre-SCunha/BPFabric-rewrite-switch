3bb06ed83b789e0def59a286c20dc41a99296a61
osboxes.org <brayils.andre@gmail.com>
2018-04-26 01:32:53 -0300

    Working Rewrite service

--- examples/rewrite.c
+++ examples/rewrite.c
@@ -43,78 +43,103 @@ uint64_t packIpAddr(uint32_t* src, uint32_t* dst);
 
 uint64_t prog(struct packet *pkt)
 {	
-
     //ARP PACK
     if (pkt->eth.h_proto == ARP_PROTOCOL) {
-
+	//Get ARP
 	struct arphdr *arp = (struct arphdr *)(((uint8_t *)&pkt->eth) + ETH_HLEN);
-	/*bpf_notify(1, &arp->ar_hrd, sizeof(uint16_t));
-        bpf_notify(1, &arp->ar_pro, sizeof(uint16_t));
-        bpf_notify(1, &arp->ar_hrdlen, sizeof(uint8_t));
-        bpf_notify(1, &arp->ar_prolen, sizeof(uint8_t));
-        bpf_notify(1, &arp->ar_op, sizeof(uint16_t));*/
 
+	//Collecting header info
 	uint8_t *src_eth, *src_ip, *dst_eth, *dst_ip;
-        uint16_t len = pkt->metadata.length;
         src_eth = ((uint8_t *)arp + ARP_HLEN);
         src_ip = src_eth + arp->ar_hrdlen;
         dst_eth = src_ip + arp->ar_prolen;
         dst_ip = dst_eth + arp->ar_hrdlen;
-	/*bpf_notify(1, src_eth, arp->ar_hrdlen);
-        bpf_notify(1, src_ip, arp->ar_prolen);
-        bpf_notify(1, dst_eth, arp->ar_hrdlen);
-        bpf_notify(1, dst_ip, arp->ar_prolen);
-        bpf_notify(1, &pkt->metadata.length, sizeof(uint16_t));*/
+	int isDstServer, isSrcServer;
+	isDstServer = isServer((uint32_t*)dst_ip);
+	isSrcServer = isServer((uint32_t*)src_ip);
 
-	if (isServer(dst_ip)){//And src not a server
+	//In Packet
+	if (isDstServer==1 && isSrcServer==0) {
 		//Hash Ip src and get corresponding server
-		uint32_t i = hashIP(src_ip);
-		uint32_t* p = &i;
-		uint32_t* g;
-		bpf_map_lookup_elem(&servers, p, &g);
-		bpf_notify(1, g, sizeof(uint32_t));
-		//make a conection key to store information
-		uint32_t key = makeConnKey(src_ip,dst_ip,g);
-		uint64_t conval = packIpAddr (src_ip,dst_ip);
-		uint64_t* conp;
-		//store ip header by key
-		bpf_map_update_elem(&conns, &key, &conval, 0);
-		bpf_map_lookup_elem(&conns, &key, &conp);
-		bpf_notify(1, (uint32_t*)conp, sizeof(uint32_t));//dst
-		bpf_notify(1, (uint32_t*)conp + 1, sizeof(uint32_t));//src
+		uint32_t key = hashIP((uint32_t*)src_ip);
+		uint32_t* server;
+		bpf_map_lookup_elem(&servers, &key, &server);
+		//Make a conection key and store header information
+		uint32_t conkey = makeConnKey((uint32_t*)src_ip, (uint32_t*)dst_ip, server);
+		uint64_t conval = packIpAddr ((uint32_t*)src_ip, (uint32_t*)dst_ip);
+		bpf_map_update_elem(&conns, &conkey, &conval, 0);
+		//Rewrite pkt with connection key as src
+		(*(uint32_t *)dst_ip) = (*server);
+		(*(uint32_t *)src_ip) = conkey;
 	}
 
-
-	//if ((*(uint32_t *)dst_ip) == server){
-            //bpf_notify(1, dst_ip, arp->ar_prolen);
-	    //(*(uint32_t *)dst_ip) = host;
-            //bpf_notify(1, dst_ip, arp->ar_prolen);
-       // }
-        //if ((*(uint32_t *)src_ip) == host){
-            //bpf_notify(1, src_ip, arp->ar_prolen);
-	    //(*(uint32_t *)src_ip) = server;
-            //bpf_notify(1, src_ip, arp->ar_prolen);
-        //}
-        //bpf_notify(1, &arppkg->ar_op + sizeof(uint16_t) + arppkg->ar_hrdlen - 4, arppkg->ar_prolen);
+	//Out Packet
+	if (isDstServer==0 && isSrcServer==1) {
+		//Retrieving header info from conkey
+		uint64_t* hdrInfo;
+		bpf_map_lookup_elem(&conns, dst_ip, &hdrInfo);
+		//Rewrite pkt correct info
+		(*(uint32_t *)dst_ip) = (*((uint32_t*)hdrInfo + 1));
+		(*(uint32_t *)src_ip) = (*(uint32_t*)hdrInfo);
+	}
     }
+
     //IP PACK
     else if (pkt->eth.h_proto == IP_PROTOCOL) {
+	//Get IP
         struct ip *ipv4 = (struct ip *)(((uint8_t *)&pkt->eth) + ETH_HLEN);
-	//if ((*(uint32_t *)&ipv4->ip_dst.s_addr) == server){
-            //bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
-	    //(*(uint32_t *)&ipv4->ip_dst.s_addr) = host;
-            //(*(uint16_t *)&ipv4->ip_sum) += ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
-            //bpf_notify(1, &ipv4->ip_dst.s_addr, sizeof(struct in_addr));
-        //}
-        //if ((*(uint32_t *)&ipv4->ip_src.s_addr) == host){
-            //bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
-	    //(*(uint32_t *)&ipv4->ip_src.s_addr) = server;
-            //(*(uint16_t *)&ipv4->ip_sum) -= ((uint16_t)server - (uint16_t)host) + ((uint16_t)(server>>16) - (uint16_t)(host>>16));
-            //bpf_notify(1, &ipv4->ip_src.s_addr, sizeof(struct in_addr));
-        //}
+
+	//Collecting header info
+	uint32_t *src_ip, *dst_ip;
+        src_ip = (uint32_t*)&ipv4->ip_src.s_addr;
+        dst_ip = (uint32_t*)&ipv4->ip_dst.s_addr;
+	uint16_t *checksum = (uint16_t *)&ipv4->ip_sum;
+	int isDstServer, isSrcServer;
+	isDstServer = isServer(dst_ip);
+	isSrcServer = isServer(src_ip);
+
+	//In Packet
+	if (isDstServer==1 && isSrcServer==0) {
+		//Hash Ip src and get corresponding server
+		uint32_t key = hashIP(src_ip);
+		uint32_t* server;
+		bpf_map_lookup_elem(&servers, &key, &server);
+		//Make a conection key and store header information
+		uint32_t conkey = makeConnKey(src_ip, dst_ip, server);
+		uint64_t conval = packIpAddr (src_ip, dst_ip);
+		bpf_map_update_elem(&conns, &conkey, &conval, 0);
+		//Rewrite pkt with connection key as src
+		uint32_t dst_val = (*dst_ip);
+		uint32_t src_val = (*src_ip);
+		(*dst_ip) = (*server);
+		(*src_ip) = conkey;
+		//Updating checksum
+		uint32_t srv_val = (*server);
+		uint32_t new_check = (uint32_t)(*checksum);
+		new_check += ((uint16_t)dst_val - (uint16_t)srv_val) + ((uint16_t)(dst_val>>16) - (uint16_t)(srv_val>>16));
+		new_check += ((uint16_t)src_val - (uint16_t)conkey ) + ((uint16_t)(src_val>>16) - (uint16_t)(conkey>>16) );
+		(*checksum) = (uint16_t)new_check + (uint16_t)(new_check>>16);
+	}
+
+	//Out Packet
+	if (isDstServer==0 && isSrcServer==1) {
+		//Retrieving header info from conkey
+		uint64_t* hdrInfo;
+		bpf_map_lookup_elem(&conns, dst_ip, &hdrInfo);
+		//Rewrite pkt correct info
+		uint32_t dst = (*dst_ip);
+		uint32_t src = (*src_ip);
+		(*dst_ip) = (*((uint32_t*)hdrInfo + 1));
+		(*src_ip) = (*(uint32_t*)hdrInfo);
+		//Update checksum
+		uint32_t new_check = (uint32_t)(*checksum);
+		new_check += ((uint16_t)dst - (uint16_t)(*dst_ip)) + ((uint16_t)(dst>>16) - (uint16_t)((*dst_ip)>>16));
+		new_check += ((uint16_t)src - (uint16_t)(*src_ip)) + ((uint16_t)(src>>16) - (uint16_t)((*src_ip)>>16));
+		(*checksum) = (uint16_t)new_check + (uint16_t)(new_check>>16);
+	}
     }
 
-    
+    //Learning Switch    
     uint32_t *out_port;
 
     // if the source is not a broadcast or multicast
@@ -144,7 +169,6 @@ int isServer(uint32_t* ip){
 	uint32_t* g;
 	while (bpf_map_lookup_elem(&servers, p, &g) != -1){
 		i++;
-		//bpf_notify(1, dst_ip, sizeof(uint32_t));
 		if ((*g) == (*ip))
 			return 1;
 	}
@@ -162,8 +186,8 @@ int hashIP(uint32_t* ip){
 int makeConnKey(uint32_t* src, uint32_t* dst, uint32_t* srv){
 	int val = (*src);
 	int res = (val>>16)*3;
-	/*val = (*dst);
-	res += (val>>16)*7;*/
+	val = (*dst);
+	res += (val>>16)*7;
 	val = (*srv);
 	res += (val>>16)*13;
 	return res*7;
